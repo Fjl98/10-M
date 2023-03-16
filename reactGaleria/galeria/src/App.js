8dde579f0160ac9b7ef05762ecd429ba1d912464
Fjl98 <116802483+Fjl98@users.noreply.github.com>
2023-03-16 13:01:44 +0100

    15_M

--- reactGaleria/galeria/src/App.js
+++ reactGaleria/galeria/src/App.js
@@ -1,19 +1,50 @@
 import React from "react";
 import { Component } from "react";
 import PhotoContextProvider from "./context/PhotoContext";
-import Header from "./Component/Header";
-import Item from "./Component/item";
-import Search from "./Component/search";
-import NotFound from "./Component/NotFound";
+import Header from "./Components/Header";
+import Item from "./Components/item";
+import Search from "./Components/search";
+import NotFound from "./Components/NotFound";
+import { Router } from "express";
+import {HashRouter, Routes, Switch, Navigate} from "react-router-dom"
 
-class App() {
-  return (
-    <PhotoContextProvider>
-      <HashRouter basename>
+class App extends Component{
+  handleSubmit = (e,history,searchInput) => {
+    e.preventDefault();
+e.currentTarget.reset();
+    let url=`/search/${searchInput}`;
+    history.push(url);
+  }
+  render(){
+    return(<PhotoContextProvider>
+      <HashRouter basename="SnapScout">
+        <div>
+        <Routes
+              render={props => (
+                <Header
+                  handleSubmit={this.handleSubmit}
+                  history={props.history}
+                />
+              )}
+            />
+<Switch>
+          <Routes exact path="/" render={()=><Navigate to="/mountain"></Navigate>}>
+          </Routes>
+
+          <Routes  path="/mountain" render={()=><Item searchTerm="mountain"></Item>}>
+          </Routes>
+          <Routes  path="/bird" render={()=><Item searchTerm="bird"></Item>}>
+          </Routes>
+          <Routes  path="/beach" render={()=><Item searchTerm="beach"></Item>}>
+          </Routes>
+          <Routes  path="/food" render={()=><Item searchTerm="food"></Item>}>
+          </Routes>
+            
+            <Routes Component={NotFound}></Routes>
+          </Switch>
+        </div>
       </HashRouter>
-    </PhotoContextProvider>
-
-  );
+    </PhotoContextProvider>)
+  }
 }
-
 export default App;
